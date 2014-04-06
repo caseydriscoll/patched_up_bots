11d184e947ff0d9113e8481ced895345f0001d7e
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-04-06 01:01:17 +0000

    Adds submenu

---
+++ class-patched-up-bots-admin-page.php
@@ -0,0 +1,10 @@
+<?php
+
+class Patched_Up_Bots_Admin_Page {
+
+	public static function render() {
+	}
+
+}
+
+?>
--- patched_up_bots.php
+++ patched_up_bots.php
@@ -8,6 +8,24 @@
  * License: GPL2
  */
 
+require_once( plugin_dir_path( __FILE__ ) . 'class-patched-up-bots-admin-page.php' );
+
 class Patched_Up_Bots {
 
+	public static function init() {
+		add_action('admin_menu', 'Patched_Up_Bots::register_patched_up_bots_menu');
+	}
+
+	public static function register_patched_up_bots_menu() {
+		add_management_page(
+			'Patched Up Bots',
+			'Generate Data',
+			'manage_options',
+			'patched-up-bots',
+			'Patched_Up_Bots_Admin_Page::render'
+		);
+	}
+
 }
+
+Patched_Up_Bots::init();
