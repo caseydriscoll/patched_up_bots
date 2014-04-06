223886fdbba417a09907d5e2220d2cd6aeab2ea7
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-04-06 00:26:51 +0000

    Init commit

---
+++ patched_up_bots.php
@@ -0,0 +1,13 @@
+<?php
+/* Plugin Name: Patched Up Bots
+ * Plugin URI:
+ * Description: A data generator and automator
+ * Version: 0.0.1
+ * Author: Casey Patrick Driscoll
+ * Author URI: http:caseypatrickdriscoll.com
+ * License: GPL2
+ */
+
+class Patched_Up_Bots {
+
+}
